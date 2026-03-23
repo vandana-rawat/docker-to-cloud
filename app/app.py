90ec4812c68c6f72dd9e130b918efebf6a755770
gurjeetkaur0005 <gk2845@nyu.edu>
2026-03-23 00:31:08 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,14 +6,14 @@ import os
 
 mongodb_host = os.environ.get('MONGO_HOST', 'localhost')
 mongodb_port = int(os.environ.get('MONGO_PORT', '27017'))
-client = MongoClient(mongodb_host, mongodb_port)    #Configure the connection to the database
-db = client.camp2016    #Select the database
-todos = db.todo #Select the collection
+client = MongoClient(mongodb_host, mongodb_port)   
+db = client.camp2016  
+todos = db.todo 
 
 app = Flask(__name__)
 title = "TODO with Flask"
 heading = "ToDo Reminder"
-#modify=ObjectId()
+
 
 def redirect_url():
 	return request.args.get('next') or \
@@ -22,7 +22,7 @@ def redirect_url():
 
 @app.route("/list")
 def lists ():
-	#Display the all Tasks
+
 	todos_l = todos.find()
 	a1="active"
 	return render_template('index.html',a1=a1,todos=todos_l,t=title,h=heading)
@@ -30,7 +30,7 @@ def lists ():
 @app.route("/")
 @app.route("/uncompleted")
 def tasks ():
-	#Display the Uncompleted Tasks
+	
 	todos_l = todos.find({"done":"no"})
 	a2="active"
 	return render_template('index.html',a2=a2,todos=todos_l,t=title,h=heading)
@@ -38,34 +38,29 @@ def tasks ():
 
 @app.route("/completed")
 def completed ():
-	#Display the Completed Tasks
+
 	todos_l = todos.find({"done":"yes"})
 	a3="active"
 	return render_template('index.html',a3=a3,todos=todos_l,t=title,h=heading)
 
 @app.route("/done")
 def done ():
-	#Done-or-not ICON
+
 	id=request.values.get("_id")
 	task=todos.find({"_id":ObjectId(id)})
 	if(task[0]["done"]=="yes"):
 		todos.update_one({"_id":ObjectId(id)}, {"$set": {"done":"no"}})
 	else:
 		todos.update_one({"_id":ObjectId(id)}, {"$set": {"done":"yes"}})
-	redir=redirect_url()	# Re-directed URL i.e. PREVIOUS URL from where it came into this one
+	redir=redirect_url()	
 
-#	if(str(redir)=="http://localhost:5000/search"):
-#		redir+="?key="+id+"&refer="+refer
+
 
 	return redirect(redir)
 
-#@app.route("/add")
-#def add():
-#	return render_template('add.html',h=heading,t=title)
 
 @app.route("/action", methods=['POST'])
 def action ():
-	#Adding a Task
 	name=request.values.get("name")
 	desc=request.values.get("desc")
 	date=request.values.get("date")
@@ -75,7 +70,6 @@ def action ():
 
 @app.route("/remove")
 def remove ():
-	#Deleting a Task with various references
 	key=request.values.get("_id")
 	todos.delete_one({"_id":ObjectId(key)})
 	return redirect("/")
@@ -88,7 +82,6 @@ def update ():
 
 @app.route("/action3", methods=['POST'])
 def action3 ():
-	#Updating a Task with various references
 	name=request.values.get("name")
 	desc=request.values.get("desc")
 	date=request.values.get("date")
@@ -99,7 +92,6 @@ def action3 ():
 
 @app.route("/search", methods=['GET'])
 def search():
-	#Searching a Task with various references
 
 	key=request.values.get("key")
 	refer=request.values.get("refer")
@@ -124,5 +116,4 @@ if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
 	debug = False if env == 'production' else True
 	app.run(host="0.0.0.0", port=5000)
-	# Careful with the debug mode..
 
